68add198e8e9f3e4d9684dd9117ed6ac12efff9e
Ta Dang Duong <tadduong2004@gmail.com>
2024-12-03 20:04:46 +0700

    fix phần đăng ký

--- src/eduverse-backend/app.js
+++ src/eduverse-backend/app.js
@@ -10,6 +10,13 @@ const courseRoutes = require('./routes/courseRoutes');
 //const materialRoutes = require('./routes/materialRoutes');
 //const userRoutes = require('./routes/userRoutes');
 //const reportRoutes = require('./routes/reportRoutes');
+//const lessonRoutes = require('./routes/lessonRoutes');
+//const assignmentRoutes = require('./routes/assignmentRoutes');
+//const submissionRoutes = require('./routes/submissionRoutes');
+//const certificateRoutes = require('./routes/certificateRoutes');
+//const reviewRoutes = require('./routes/reviewRoutes');
+//const paymentRoutes = require('./routes/paymentRoutes');
+//const notificationRoutes = require('./routes/notificationRoutes');
 
 const app = express();
 
@@ -24,5 +31,12 @@ app.use('/api/courses', courseRoutes);
 //app.use('/api/materials', materialRoutes);
 //app.use('/api/users', userRoutes);
 //app.use('/api/reports', reportRoutes);
+//app.use('/api/lessons', lessonRoutes);
+//app.use('/api/assignments', assignmentRoutes);
+//app.use('/api/submissions', submissionRoutes);
+//app.use('/api/certificates', certificateRoutes);
+//app.use('/api/reviews', reviewRoutes);
+//app.use('/api/payments', paymentRoutes);
+//app.use('/api/notifications', notificationRoutes);
 
 module.exports = app;
