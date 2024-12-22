ac8059f3ce5a9b2e7f6a2886aa6a97db44d4ca50
nguyenngoctung75 <tungit1huster@gmail.com>
2024-12-22 20:10:00 +0700

    update

---
+++ src/eduverse-frontend/src/components/Lesson/LessonShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LessonCreate.css';
+
+const LessonShow = () => {
+    return (
+        <div className="lesson-show">
+            <h1>Lesson Show</h1>
+        </div>
+    )
+}
+
+export default LessonShow;
